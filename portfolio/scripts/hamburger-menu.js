edc209a8e0762506afbd4a557e218bc844223787
Andre da Silva <apdasil@emory.edu>
2024-11-28 20:46:11 -0500

    Changed "href: '../" to "href: './"

--- portfolio/scripts/hamburger-menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.querySelector('.hamburger');
-    const navLinks = document.querySelector('.nav-links-container');
-
-    hamburger.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-    });
-});
